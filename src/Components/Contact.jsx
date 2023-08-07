179ab9bfba77cc136d169337c78740ab9b95f82d
Kumghato khala <kumghatokhala11@gmail.com>
2023-08-07 23:40:02 +0530

    Contact page update

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
                 <hr className='w-1/2 sm:mt-5 m-auto border-[var(--primaryColor)] border' />
             </div>
             <div className='container flex mt-20 mb-10 m-auto'>
-                <div className='border my-3 lg:flex m-auto w-[80%]'>
+                <div className='border my-3 lg:flex m-auto w-[90%] md:w-[80%]'>
                     <div className='lg:w-1/2'>
                         <form className='flex flex-col p-10 gap-2'>
                             <input className='border p-3 rounded' type='text' name='fullname' placeholder='Fullname' />
@@ -23,17 +23,17 @@ const Contact = () => {
                     </div>
                     <hr className='w-3/5 lg:w-0 h-0 lg:h-3/5 m-auto border border-gray-400' />
                     <div className='lg:w-1/2'>
-                        <div className='p-10 flex flex-col'>
-                            <h5 className='text-sm md:text-lg text-start mb-10 font-bold m-auto'>
+                        <div className='text-[0.7rem] md:text-base p-10 flex flex-col'>
+                            <h5 className='text-start mb-10 font-bold m-auto'>
                                 For any inquiries, please contact us at
                             </h5 >
                             <div className='my:2 lg:my-4'>
                                 <div className='flex items-center'>
-                                    <FaPhoneAlt className='text-xl lg:text-3xl me-4' />
+                                    <FaPhoneAlt className='text-lg lg:text-3xl me-2 md:me-3' />
                                     <p className='mb-0'>+91 986 269 4100</p>
                                 </div>
                                 <div className='flex items-center mt-5'>
-                                    <FaRegEnvelope className='text-xl lg:text-3xl me-4' />
+                                    <FaRegEnvelope className='text-lg lg:text-3xl me-2 md:me-3' />
                                     <p className='mb-0'>support@givetheneedy.org.in</p>
                                 </div>
                             </div>
